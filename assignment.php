acd5d5631934b5f08772b9afabff8d29a0b025f3
Mariya Biju <bijumariya38@gmail.com>
2025-06-19 18:01:07 +0530

    assighnment operator

---
+++ assignment.php
@@ -0,0 +1,21 @@
+<?php
+    $x=10;
+    echo "= assighnment operator \n";
+    echo "value of x is $x \n";
+    $y=$x;
+    echo "after operation ";
+    echo "the value of y= $y ";
+    echo "\n";
+    $x+=$y;
+    print $x;
+    echo "\n";
+    $x-=$y;
+    echo $x;
+    echo "\n";
+    $x*=$y;
+    echo $x;
+    echo "\n";
+    $x/=$y;
+    echo $y;
+ ?>
+

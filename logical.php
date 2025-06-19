b8ceb822405757b91795e458d35c05fc78ed9945
Mariya Biju <bijumariya38@gmail.com>
2025-06-19 17:58:38 +0530

    logical

---
+++ logical.php
@@ -0,0 +1,21 @@
+<?php
+    $x=12;
+    $y=23;
+    if ($x<45 && $y>5)
+    {
+         echo "and\n";
+    }
+    
+    if ($x<45 or $y>500)
+    {
+        echo "or\n";
+    }
+    if ($x<45 xor $y>5)
+    {
+        echo "xor\n";
+    }
+    if ($x<45 xor $y>500)
+    {
+        echo "xor\n";
+    }
+?>

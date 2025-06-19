bb5412c3a3dac904da603b0c4e11a7298ca00ed7
Mariya Biju <bijumariya38@gmail.com>
2025-06-19 18:02:45 +0530

    increment derement operator

---
+++ incrementDecrement.php
@@ -0,0 +1,15 @@
+<?php
+    $x=10;
+    echo ++$x,"\n";//increment first then return
+
+    $q=10;
+    echo $q++,"\n";//return q then increment
+    echo "after vqlue of q is ".$q."\n";
+
+    $w=5;
+    echo --$w,"\n";//decrement first then return 
+
+    $r=8;
+    echo $r--,"\n";//return firts  then decrement
+    echo "after value of r is ",$r;
+?>

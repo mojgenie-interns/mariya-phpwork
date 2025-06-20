58dbaf367639190817791b7c9055c0b664f6a9c5
Mariya Biju <bijumariya38@gmail.com>
2025-06-20 11:55:04 +0530

    switch

---
+++ switch.php
@@ -0,0 +1,15 @@
+<?php
+
+    $x="red";
+    switch ($x)
+    {
+        case "red":
+            echo "red is your color";
+            break;
+        case "pink":
+            echo "pink is your color";
+            break;
+        default:
+            echo "the entered color not in the list";
+    }
+?>

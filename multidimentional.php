d6fce1b97cfe2a5dd7ebfc8729a51d7432110e80
Mariya Biju <bijumariya38@gmail.com>
2025-06-20 16:26:15 +0530

    multidimentional array

---
+++ multidimentional.php
@@ -0,0 +1,7 @@
+<?php
+    $fruits=array(array("orange",22,33),array("apple",11,03));
+    echo $fruits[0][0],"\n";
+    echo $fruits[0][1],"\n";
+    echo $fruits[0][2],"\n";
+    echo $fruits[1][0],"\n";
+?>

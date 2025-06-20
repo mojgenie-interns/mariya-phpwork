a5e2fe3e976b78f40e870c18f8ba8d915d69a657
Mariya Biju <bijumariya38@gmail.com>
2025-06-20 10:29:10 +0530

    if statements

---
+++ ifStatements.php
@@ -0,0 +1,17 @@
+<?php
+    //simple if statement
+    $a=readline("enter a number");
+    if ($a<20)
+    {
+        echo "the number is less than 20\n";
+    }
+    //if else
+    if ($a==20)
+    {
+        echo "the entered number is 20";
+    }
+    else
+    {
+        echo "the number is not equal to 20";
+    }
+?>

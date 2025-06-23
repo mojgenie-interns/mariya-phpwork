ab1b907d20a8ded498e4cbd89b3174442a5726ca
Mariya Biju <bijumariya38@gmail.com>
2025-06-23 12:11:34 +0530

    Constants in opps

---
+++ ConstantsInOOP.php
@@ -0,0 +1,10 @@
+<?php
+    class Demo
+    {
+        //creating a constant using constant keyword
+        const HELLO="Hi there this is a demo message for constants in oops";
+
+    }
+
+    echo Demo ::HELLO;
+?>

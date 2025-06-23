c22d35bb9563ecd66c6d9b11b966c769bcd23b86
Mariya Biju <bijumariya38@gmail.com>
2025-06-23 15:48:54 +0530

    Abstact classes

---
+++ Abstract.php
@@ -0,0 +1,34 @@
+<?php
+    //creating an abstact class
+    abstract class Vehicle{
+        public $brand;
+        abstract public function startEngine();
+        public function showBrand($brand){
+            $this->brand=$brand;
+            echo "Brand: $this->brand \n";
+        }
+        
+    }
+
+    //creating child class$brand
+    class Car extends Vehicle {
+        public function startEngine()
+        {
+            echo " Starting the car engine\n";
+        }
+
+    }
+    class Bike extends Vehicle{
+        public function startEngine()
+        {
+            echo " Starting the bike engine\n";
+
+        }
+    }
+    $obj1=new Car();
+    $obj2=new Bike ();
+    $obj1->showBrand("Toyota");
+    $obj1->startEngine();
+    $obj2->showBrand( "Yamaha");
+    $obj2->startEngine();
+?>

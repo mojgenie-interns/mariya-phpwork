afdc2bf2aa2d86454679ee1c9fa892fea0b84bd4
Mariya Biju <bijumariya38@gmail.com>
2025-06-23 11:23:32 +0530

    classes

---
+++ Classes.php
@@ -0,0 +1,14 @@
+<?php
+// Creating a class
+class Name {
+    public $name;
+
+    public function displayName($name) {
+        $this->name = $name;
+        echo "hii $this->name"; 
+    }
+}
+
+$object1 = new Name();
+$object1->displayName("mariya");
+?>

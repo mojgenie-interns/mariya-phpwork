18d0094677aad5dba0d1f372ab066f5f776a5e2e
Mariya Biju <bijumariya38@gmail.com>
2025-06-25 10:12:44 +0530

    simple program

--- Library.php
+++ Library.php
@@ -33,7 +33,7 @@
     trait Logger
     {
         function logActions($action){
-            echo "[] : $action\n";
+            echo " $action\n";
         }
     }
 
@@ -51,7 +51,85 @@
 
         public function borrowItem()
         {
-            
-        }
+           
+            $this->logActions("Borrowed the book --$this->title \n");
+     
+       }
+
+       //implement the interface 
+
+       public function returnItem()
+       {
+        $this-> logActions("Returnrd the book--$this->title\n");
+       }
     }
 
+        class DVD extends LibraryItem implements Borrowable{
+
+            //trait 
+
+            use Logger;
+            public $type;
+            public $name;
+
+            //constructor
+            function __construct($type,$name)
+        
+            {
+               $this->type = $type;
+               $this->name = $name; 
+            }
+
+            public function getDetails()
+            {
+                echo " DVD type is : $this->type \n";
+                echo " Name of the DVD is $this->name \n";
+
+            }
+
+            public function borrowItem()
+            {
+                $this->logActions("Borrowed the DVD $this->name \n");
+
+            }
+
+            public function returnItem()
+            {
+                $this->logActions("Returned the DVD $this->name\n");
+
+            }
+        }
+        //user need to enter the title ,name,author and type
+
+        $title = readline("Enter the title of the book :\n");
+
+        $author = readline("Enter the author of the book :\n");
+
+        $type = readline("Enter the type of DVD :\n");
+
+        $name = readline("Enter the name of the DVD : \n");
+
+
+         //creting instance of book
+
+        $book = new Book($title,$author);
+
+        //creating the instance of DVD
+
+        $dvd = new DVD($type,$name);
+        
+        
+        $ques = readline("type 1. for borrow    2.for return \n");
+
+        if ($ques==1){
+
+            $book->borrowItem();
+            $book->getDetails();
+        }
+
+        else {
+            $dvd->returnItem();
+            $dvd->getDetails();
+        }
+
+?>

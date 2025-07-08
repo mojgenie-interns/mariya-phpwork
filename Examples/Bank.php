212b6ddd24dea0a398d7e82ce687258ffdb054df
Mariya Biju <bijumariya38@gmail.com>
2025-07-08 17:21:14 +0530

    not completed

---
+++ Examples/Bank.php
@@ -0,0 +1,60 @@
+<?php
+    class BankAccountSimulator{
+
+        public $balance;
+        public $accountNumber;
+        public $accountHolder;
+        public $dataFile ='accounts.json';
+
+        function __construct()
+        {
+            $this->balance  = 0;
+        }
+
+        public  function generateAccountNumber() {
+                $file = "account_counter.txt";
+
+                if (!file_exists($file)) {
+                    file_put_contents($file, 1);
+                }
+
+                $current = (int) file_get_contents($file);
+                $accountNumber = "ACC" . str_pad($current, 6, "0", STR_PAD_LEFT);
+                file_put_contents($file, $current + 1);
+
+                return $accountNumber;
+        }
+
+        public function createAccount(){
+
+                $this->accountHolder = readline("Enter your name: ");
+                $this->accountNumber = $this->generateAccountNumber();
+                $this->balance = 0;
+
+                $newAccount = [
+                    "name" => $this->accountHolder,
+                    "account_number" => $this->accountNumber,
+                    "balance" => $this->balance
+                ];
+
+                $accounts = [];
+
+                if (file_exists($this->dataFile)) {
+                    $json = file_get_contents($this->dataFile);
+                    $accounts = json_decode($json, true);
+                }
+
+                $accounts[] = $newAccount;
+
+                file_put_contents($this->dataFile, json_encode($accounts, JSON_PRETTY_PRINT));
+                echo "Account Created\n";
+
+        }
+
+        public function depositeMoney(){
+
+            $acc = readline("Enter Account NUMber :\n");
+        }
+
+
+    }

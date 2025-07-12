abfa597adc99e735e8ce9d4176fb8e3031b20fbe
Mariya Biju <bijumariya38@gmail.com>
2025-07-12 14:10:29 +0530

    contact management

--- Examples/ContactManagement.php
+++ Examples/ContactManagement.php
@@ -1,61 +1,87 @@
 <?php
 
-class ContactManagement{
+class ContactManagement {
 
-        public $name;
-        public $contactNumber;
+    public $contacts = [];
 
-
-        
-
-
-        public function menu(){
-
-            echo "==========WELCOME=========\n";
-
+    public function menu() {
+        while (true) {
+            echo "========== WELCOME =========\n";
             echo "1. Add Contact\n";
             echo "2. View Contacts\n";
             echo "3. Delete Contact\n";
             echo "4. Exit\n";
-            $option = readline("Enter your choice :\n");
-            if ($option == 1){
+            $option = readline("Enter your choice: ");
 
-                $this->addContact();
-
-            }elseif($option == 2){
-
-                $this->viewContact();
-
-            }elseif($option == 3){
-
-                $this->deleteContact();
-
-            }elseif ($option == 4){
-
-                echo "Good Bye";
-                exit;
-            }else{
-
-                echo "invalid choice\n";
-
+            switch ($option) {
+                case 1:
+                    $this->addContact();
+                    break;
+                case 2:
+                    $this->viewContact();
+                    break;
+                case 3:
+                    $this->deleteContact();
+                    break;
+                case 4:
+                    echo "Goodbye!\n";
+                    exit;
+                default:
+                    echo "Invalid choice. Try again.\n";
             }
+        }
+    }
 
+    public function addContact() {
+        $name = readline("Enter Name: ");
+        $number = readline("Enter Contact Number: ");
 
+        if (strlen($number) != 10){
+
+            echo "Enter 10 digit number:\n";
+            echo "Contact not saved\n";
+            return;
+        }
+        $this->contacts[] = [
+            'name' => $name,
+            'number' => $number
+        ];
+        echo "Contact added successfully!\n";
+    }
+
+    public function viewContact() {
+        if (empty($this->contacts)) {
+            echo "No contacts available.\n";
+            return;
         }
 
-        public function addContact(){
+        echo "---- Contact List ----\n";
+        foreach ($this->contacts as $index => $contact) {
+            echo ($index + 1) . ". Name: {$contact['name']}, Number: {$contact['number']}\n";
+        }
+    }
 
+    public function deleteContact() {
+        $this->viewContact();
+        if (empty($this->contacts)) {
 
+            echo "No contact to delete\n";
+            return;
         }
 
-        public function viewContact(){
+        $index = readline("Enter contact number to delete : ");
+        $realIndex = $index - 1;
 
+        if (isset($this->contacts[$realIndex])) {
+            unset($this->contacts[$realIndex]);
+          
+            $this->contacts = array_values($this->contacts);
+            echo "Contact deleted successfully.\n";
+        } else {
+            echo "Invalid index.\n";
         }
-
-        public function deleteContact(){
-
-        }
-
-
-
+    }
 }
+
+$object = new ContactManagement();
+$object->menu();

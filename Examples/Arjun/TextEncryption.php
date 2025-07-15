1b6fb6134b20e42b1fd78e55fc4860881f035413
Mariya Biju <bijumariya38@gmail.com>
2025-07-15 12:11:30 +0530

    text encryption

--- Examples/Arjun/TextEncryption.php
+++ Examples/Arjun/TextEncryption.php
@@ -6,10 +6,33 @@ class Encryption{
 
     function __construct()
     {
-        $this->text = readline("Enter the word:");
+        $this->text = strtoupper(readline("Enter the word:"));
 
     }
 
-    
+    public function textEncryption(){
+
+        $shift = rand(3,100);
+
+        $arr = str_split($this->text);
+        $cipherText = ''; 
+
+        foreach($arr as $char){
+        
+            $position = ord($char) - 65;  
+            $shifted = ($position + $shift) % 26;
+            $newChar = chr($shifted + 65);
+
+            $cipherText .= $newChar;
+
+        }
+
+        echo "ENCRYPTED WORD = $cipherText \n";
+        echo "KEY = $shift ";
+
+    }
 
 }
+
+$object = new Encryption();
+$object->textEncryption();
